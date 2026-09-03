dfabf18b490aa4e8f47d3f3b4f5a7f1555e021ea
amarillo07 <amarillolindo07@gmail.com>
2026-09-02 21:28:26 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -20,8 +20,8 @@ export default defineConfig({
         background_color: "#131B29",
         display: "standalone",
         orientation: "portrait",
-        start_url: "/finanzas-mx-app/",
-        scope: "/finanzas-mx-app/",
+        start_url: "/Finzalu/",
+        scope: "/Finzalu/",
         icons: [
           { src: "icons/icon-192.png", sizes: "192x192", type: "image/png" },
           { src: "icons/icon-512.png", sizes: "512x512", type: "image/png" },
